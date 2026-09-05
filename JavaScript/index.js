45ec145116a74c4458de62d917ea0e7e7e382806
Gabriel Amado dos Santos <zarbor692@gmail.com>
2026-09-05 18:16:41 -0300

    update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',          
-  password: '@Bielzinho2007',
+  password: 'Senha do server', n vou deixar aqui segurança ne kkkkkk
   database: 'servix'   
 });
 
